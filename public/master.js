3a8432a6b73998831dd69efd6adc3e07328a0c57
Academicdons <gichengo.wangui29@gmail.com>
2022-09-20 11:44:35 +0300

    first commit

---
+++ public/master.js
@@ -0,0 +1,19 @@
+
+var GRID_HEIGHT = 40;
+var GRID_WIDTH = 60; 
+var BLOCK_SIZE = 15;
+var DIRECTIONS = {
+    up: 0,
+    right: 1,
+    down: 2,
+    left: 3
+}
+
+var COL_CHANGE = [0, 1, 0, -1];
+var ROW_CHANGE = [-1, 0, 1, 0];
+            
+var GAME_SPEEDS = [
+    {name: "Slow", speed: 200},
+    {name: "Medium", speed: 140},
+    {name: "Fast", speed: 80}
+];
